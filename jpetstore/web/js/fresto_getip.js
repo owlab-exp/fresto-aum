05f3288cdfc75c3dd044cc61d45cdacccf110477
Nemo HunJae Lee <nemo.hj.lee@gmail.com>
2013-08-27 04:57:30 +0900

    Minor chagnes for beautification

--- jpetstore/web/js/fresto_getip.js
+++ jpetstore/web/js/fresto_getip.js
@@ -1,3 +1,14 @@
+
+/* gitip.js - Version 0.5
+ *
+ * HnJae Lee
+ *
+ */
+
+/**
+ * This is a part of codes to know client IP address
+ */
+
 		var clientIP = "no IP Address";
 		function getip(json){
 			//alert(json.host);
