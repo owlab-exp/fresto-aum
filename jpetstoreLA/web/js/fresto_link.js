8799aa66a525a002f937be2e7ad35938b58c0e31
Nemo HunJae Lee <nemo.hj.lee@gmail.com>
2013-09-23 23:50:43 +0900

    Refine variable names

--- jpetstoreLA/web/js/fresto_link.js
+++ jpetstoreLA/web/js/fresto_link.js
@@ -3,18 +3,19 @@ function interceptClick(e) {
 		var target = this.target;
 
 		var callUuid = randomUUID();
-		var beforeTimeStamp = new Date().getTime();
-		var preCall = {
-			stage: 'beforeCall',
+		var request_timestamp = new Date().getTime();
+		var request_event = {
+			stage: 'HTTP_REQUEST',
+			uuid: callUuid,
 			clientId: clientIP,
 			currentPlace: window.location.href,
-			uuid: callUuid,
 			targetUrl: href,
-			timestamp: beforeTimeStamp
+			method: 'GET',
+			requestTimestamp: request_timestamp
 		}
 
 
-		$.post('http://fresto1.owlab.com:9999/feedUIEvent',preCall); 
+		$.post('http://fresto1.owlab.com:9999/feedUIEvent',request_event); 
 
 		$.ajax({
 			type: 'GET',
@@ -23,18 +24,20 @@ function interceptClick(e) {
 			},
 			url: href, 
 			success: function(data) { 
-				var afterTimeStamp = new Date().getTime();
-				var afterCall = {
-					stage: 'afterCall',
+				var response_timestamp = new Date().getTime();
+				var response_event = {
+					stage: 'HTTP_RESPONSE',
+					uuid: callUuid,
 					clientId: clientIP,
 					currentPlace: window.location.href,
-					uuid: callUuid,
 					targetUrl: href,
-					timestamp: afterTimeStamp,
-					elapsedTime: (afterTimeStamp - beforeTimeStamp),
+					method: 'GET',
+					requestTimestamp: request_timestamp,
+					responseTimestamp: response_timestamp,
+					elapsedTime: (response_timestamp - reqeust_timestamp),
 					httpStatus: 200
 				}
-				$.post('http://fresto1.owlab.com:9999/feedUIEvent',afterCall); 
+				$.post('http://fresto1.owlab.com:9999/feedUIEvent', response_event); 
 				window.history.pushState('Programmed State','JPetStore', href);
 				$('body').html(data); 
 				//document.write(data);
